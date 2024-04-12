4af0e431f4201e20308a8f7472bf7262e2f7b1a2
Raito Bezarius <masterancpp@gmail.com>
2024-04-12 20:05:13 +0200

    feat: upgrade to lean v4.7
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- lakefile.lean
+++ lakefile.lean
@@ -7,13 +7,6 @@ require base from git
 package «AvlVerification» where
   -- add package configuration options here
 
+@[default_target]
 lean_lib «AvlVerification» where
   -- add library configuration options here
-
-@[default_target]
-lean_exe «avlverification» where
-  root := `Main
-  -- Enables the use of the Lean interpreter by the executable (e.g.,
-  -- `runFrontend`) at the expense of increased binary size on Linux.
-  -- Remove this line if you do not need such functionality.
-  supportInterpreter := true
