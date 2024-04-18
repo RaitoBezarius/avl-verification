5289b86a5c14ec99311424022a867872173edce4
Raito Bezarius <masterancpp@gmail.com>
2024-04-18 14:34:36 +0200

    chore: nixify the project
    So that I stop doing `nix run` one-offs!
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ sources ? import ./npins
+, pkgs ? import sources.nixpkgs {}
+, lib ? pkgs.lib
+, aeneas-src ? sources.aeneas
+, aeneas ? (import aeneas-src {}).packages.x86_64-linux.aeneas
+, charon ? (import aeneas-src {}).packages.x86_64-linux.charon
+}:
+{
+  shell = pkgs.mkShell {
+    buildInputs = [
+      aeneas
+      charon
+      pkgs.rustup
+      pkgs.npins
+    ];
+  };
+}
