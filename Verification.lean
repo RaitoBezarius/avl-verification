ff0014e67af3962d128daf82b587d5875c988d7d
Raito Bezarius <masterancpp@gmail.com>
2024-05-13 18:29:11 +0200

    feat: add Height & Rotate to verification
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- Verification.lean
+++ Verification.lean
@@ -2,3 +2,5 @@ import Verification.Extracted
 -- import Verification.Insert ; TODO: the insert_loop does not exist anymore!
 import Verification.Find
 import Verification.Order
+import Verification.Height
+import Verification.Rotate
